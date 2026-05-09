72fae86192fdf4239c7bbca5d285e8999096c25e
Shaedow2000 <shaedow2000@proton.me>
2026-05-09 19:46:41 +0100

    Implemenet middlewares.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import type { Express } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 async function start(): Promise<void> {
   app.listen(1000, (): void => {
