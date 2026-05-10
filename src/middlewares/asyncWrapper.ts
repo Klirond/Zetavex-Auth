7f34d753ed51c48c14016e52342416c5ba11b5ed
Shaedow2000 <shaedow2000@proton.me>
2026-05-10 11:52:05 +0100

    Create async function wrapper.

---
+++ src/middlewares/asyncWrapper.ts
@@ -0,0 +1,19 @@
+import { Response, Request, NextFunction } from "express";
+
+const wrapper: Function = (
+  f: Function,
+): ((res: Response, req: Request, next: NextFunction) => Promise<void>) => {
+  return async (
+    res: Response,
+    req: Request,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      await f(res, req, next);
+    } catch (err: Error | unknown) {
+      next(err);
+    }
+  };
+};
+
+export default wrapper;
